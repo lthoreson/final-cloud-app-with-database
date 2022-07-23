d25ba8a16883c269ac5c706735926df0321aee6f
lthoreson <lgeckot2002@msn.com>
2022-07-23 22:27:38 +0000

    course detail template edits

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -18,7 +18,7 @@ class LessonInline(admin.StackedInline):
 
 # Register your models here.
 class CourseAdmin(admin.ModelAdmin):
-    inlines = [LessonInline]
+    inlines = [LessonInline, QuestionInline]
     list_display = ('name', 'pub_date')
     list_filter = ['pub_date']
     search_fields = ['name', 'description']
@@ -26,10 +26,6 @@ class CourseAdmin(admin.ModelAdmin):
 
 class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
-    inlines = [QuestionInline]
-
-class QuestionAdmin(admin.ModelAdmin):
-    inlines = [ChoiceInline]
 
 # <HINT> Register Question and Choice models here
 
@@ -37,5 +33,5 @@ admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
-admin.site.register(Question, QuestionAdmin)
+admin.site.register(Question)
 admin.site.register(Choice)
